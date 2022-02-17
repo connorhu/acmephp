00bea79a49f210f0b588f2d7e182bfd3d27b0512
Karoly Gossler <connor@connor.hu>
2022-02-17 09:59:34 +0100

    added exceptions for DirectAdmin solver

---
+++ src/Core/Exception/Challenge/Dns/DirectAdminDnsControlException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AcmePhp\Core\Exception\Challenge\Dns;
+
+class DirectAdminDnsControlException extends \Exception
+{
+
+}
---
+++ src/Core/Exception/Challenge/Dns/ZoneNotManagedByDirectAdminException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AcmePhp\Core\Exception\Challenge\Dns;
+
+class ZoneNotManagedByDirectAdminException extends \Exception
+{
+
+}
